2198f97109153f7c1382b65069d8102b2b134253
Gabriel Seals <sealsgj@gmail.com>
2020-03-01 20:20:24 -0600

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's talk secrets. What's yours?");
+            var secret = Console.ReadLine();
+
+            var maskedString = new String('X', secret.Length - 4) + secret.Substring(secret.Length - 4);
+
+            Console.WriteLine(maskedString);
+            Console.ReadLine();
+        }
+    }
+}
